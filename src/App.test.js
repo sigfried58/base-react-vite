11656e476a144cf5596d28c994bd656a16b43abb
jimmy ibanez <jimmy.ibanez@outlook.com>
2023-06-21 00:11:55 -0400

    chore: simple app

---
+++ src/App.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+
+test('can receive a new user and show it on a list', async () => {
+  const user = userEvent.setup();
+  render(<App />);
+
+  const nameInput = screen.getByRole('textbox', {
+    name: /name/i,
+  });
+  const emailInput = screen.getByRole('textbox', {
+    name: /email/i,
+  });
+  const button = screen.getByRole('button');
+
+  await user.click(nameInput);
+  await user.keyboard('jane');
+  await user.click(emailInput);
+  await user.keyboard('jane@jane.com');
+
+  await user.click(button);
+
+  const name = screen.getByRole('cell', { name: 'jane' });
+  const email = screen.getByRole('cell', { name: 'jane@jane.com' });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
+});
